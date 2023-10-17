cea031efb3e84857cbe2c6e4348b3dc84b40f460
atknsntrk <atakan.sent@gmail.com>
2023-10-18 00:53:53 +0200

    display cached list

--- src/render.js
+++ src/render.js
@@ -1,14 +1,33 @@
+const input = document.getElementById("inputText")
+const submit = document.getElementById("inputSubmit")
+const form = document.getElementById("submitForm")
+const items = document.getElementById("item-list")
+const log = document.getElementById("values")
+
 function prepareApp() {
-    const input = document.getElementById("inputText")
-
     input.addEventListener("input", updateValue)
-
+    submit.addEventListener("submit", addValue)
+    form.addEventListener("submit", addValue)
 }
+
+const courses = []
+let currentText = ""
 
 function updateValue(event) {
     console.log(event)
-    const log = document.getElementById("values")
     log.textContent = event.target.value
+    currentText = event.target.value
+}
+
+function addValue(event) {
+    event.preventDefault();
+    courses.push(currentText)
+    currentText = ""
+    input.value = ""
+    console.log(courses)
+    log.textContent = ""
+    const HTML = courses.map(item => `<li>${item}</li> ` ).join('');
+    items.innerHTML = '<ul>' +HTML+ '</ul>'
 }
 
 console.log("in renderer")
